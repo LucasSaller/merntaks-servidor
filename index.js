0ad4915036eaaafccc3a32a73fe44989db2781df
LucasSaller <sallerlucas@gmail.com>
2021-04-05 13:51:22 -0300

    cambio en el index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar rutas
 
@@ -24,8 +24,8 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 //arrancar la app
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`listening on port ${port}`);
 });
 
 // definir la pag principal
